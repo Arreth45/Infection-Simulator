5189d7887838660700a21ff189861e0d286a8a7d
Arreth45 <arreth45@gmail.com>
2016-03-31 12:53:10 -0400

    created initial scripts

---
+++ Assets/Scripts/generator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class generator : MonoBehaviour {
+    public double maxPeople = 10;
+    private double currentPeople;
+    private double alivePeople;
+    private double infectedPeople;
+    private double deadPeople;
+    
+
+	// Use this for initialization
+	void Start () {
+        //spawn max people
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //keep people at max
+	
+	}
+}
---
+++ Assets/Scripts/person.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class person : MonoBehaviour {
+    private bool isInfected,isAlive,isDead;
+    private float runspeed;
+
+	// Use this for initialization
+	void Start () {
+        
+        
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(isAlive){
+            //code to run from "zombies"
+        }
+        
+        if(isInfected){
+            //code to chase alive people
+           //after some time the zombies will die off
+        }
+        
+        if(isDead){
+            Destroy(gameObject);         
+        }     	
+	}
+}
